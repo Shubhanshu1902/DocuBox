71a5e13e067ed639c06b81981c987a191507e64e
Shubhanshu1902 <shubhanshu1902@gmail.com>
2022-02-12 21:11:14 +0530

    main screen

--- src/homepage/components/Centericon.js
+++ src/homepage/components/Centericon.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./centericon.css";
+import upperlock from "../../images/upperlock.png";
+import lowerlock from "../../images/lowerlock.png";
 
 const Centericon = () => {
     return (
         <div>
-            <div className="centericon" />
-            <div className="top-lock">
-                
+            <div className="centericon">
+                <div className="upperlock">
+                    <img src={upperlock} alt="upperlock" />
+                </div>
+
+                <div className="docubox">
+                    DocuBox
+                    <div className="box" />
+                </div>
+
+                <div className="intro">Your document locker</div>
+                <div className="intro-sol">solution</div>
+
+                <div className="lowerlock">
+                    <img src={lowerlock} alt="lowerlock" />
+                </div>
             </div>
         </div>
     );
