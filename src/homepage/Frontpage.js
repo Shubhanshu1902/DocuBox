cb5a3dc15162ce2253d43c4346e09c44075be2e2
Shubhanshu1902 <shubhanshu1902@gmail.com>
2022-02-13 14:19:28 +0530

    css change

--- src/homepage/Frontpage.js
+++ src/homepage/Frontpage.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Bggradient from "./components/Bggradient";
 import Centericon from "./components/Centericon";
-import Navbar_ from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import "./frontpage.css";
+import "./components/bggradient.css"
 
 const Frontpage = () => {
     return (
         <div className="frontpage">
-            <Bggradient />
-            <Navbar_ />
-            <Centericon />
+            {/* <div className="bggradient"> */}
+
+            {/* </div> */}
+            {/* <Bggradient /> */}
+            <Navbar />
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height:'100%', width:'100%'}}>
+                <Centericon />
+            </div>
         </div>
     );
 };
--- src/homepage/components/Centericon.js
+++ src/homepage/components/Centericon.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./centericon.css";
 import upperlock from "../../images/upperlock.png";
 import lowerlock from "../../images/lowerlock.png";
 
+const cirleradius = 947/1080 * window.innerHeight;
+
 const Centericon = () => {
+
+    const [radius,setRadius] = useState(radius);
+
+    
+    while(true && )
+
+
+    const BoxText = () => {
+        return (
+            <div className="box">Box</div>
+        )
+    }
     return (
-        <div>
-            <div className="centericon">
-                <div className="upperlock">
-                    <img src={upperlock} alt="upperlock" />
+        <div style={{ width: cirleradius, height: cirleradius, display: 'flex', alignItems: 'center', justifyContent: 'center', }}>
+            <div className="centericon" style={{ alignItems: 'center', flexDirection: 'column', justifyContent: 'center' }}>
+                <img className="lock" src={upperlock} alt="upperlock" />
+                
+                <div className="docubox" style={{ display: 'inline-flex', marginTop: 20 }}>
+                    <div >Docu</div>
+                    <BoxText />
                 </div>
 
-                <div className="docubox">
-                    DocuBox
-                    <div className="box" />
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginBottom: 20 }} >
+                    <div className="intro">Your document locker</div>
+                    <div className="intro">solution</div>
                 </div>
 
-                <div className="intro">Your document locker</div>
-                <div className="intro-sol">solution</div>
-
-                <div className="lowerlock">
-                    <img src={lowerlock} alt="lowerlock" />
-                </div>
+                <img className="lock" src={lowerlock} alt="lowerlock" />
             </div>
         </div>
     );
